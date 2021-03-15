d6e03e2630d3b8555307f165133e25a6a57b4ac8
sponberry <74988392+sponberry@users.noreply.github.com>
2021-03-15 19:32:28 +0000

    Added Bootstrap, Google fonts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
+      <div className="container">
+        <Weather />
 
-      <footer>
-        Coded by Abi 👩🏻‍💻 | <a 
-        href="https://github.com/sponberry/shecodesreact-final-project"
-        target="_blank"
-        link="noreferrer">
-          on GitHub
-        </a>
-      </footer>
+        <footer>
+          Coded by Abi 👩🏻‍💻 | <a 
+          href="https://github.com/sponberry/shecodesreact-final-project"
+          target="_blank"
+          link="noreferrer">
+            on GitHub
+          </a>
+        </footer>
+        </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather () {
+    return (
+        <div>
+          <h1 className="cityName">React Weather App</h1>
+        </div>
+    )
+}
