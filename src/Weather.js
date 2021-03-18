45ec214fca13e934e675e03b473fa5f7ead0b43a
sponberry <74988392+sponberry@users.noreply.github.com>
2021-03-18 20:39:45 +0000

    Made current location button functional, commented all code in Weather.js for clarity

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./Weather.css";
 import WeatherDisplay from "./WeatherDisplay";
 import FiveDay from "./FiveDay";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
+
+// css imports
+import "./Weather.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 
 export default function Weather (props) {
@@ -13,6 +15,7 @@ export default function Weather (props) {
   let [city, setCity] = useState(props.defaultCity);
   let [unit, setUnit] = useState("metric")
 
+  // creates object with all req data from todays weather, sets ready to true
   function getData(response) {
     setWeatherData({
       ready: true,
@@ -28,6 +31,7 @@ export default function Weather (props) {
     })
   }
 
+  // creates array of lists with req data for 5 day forecast
   function logData(response) {
     let hourlyForecasts = response.data.list;
     let collectWeatherData = []
@@ -47,7 +51,9 @@ export default function Weather (props) {
     setFiveDayWeatherData(collectWeatherData);
   }
 
+  // makes api call for weather now and five day forecast using current city and unit states
   function search() {
+    console.log(city);
     let apiKey = "d022a7cace86a431e5ba6e5fd2caf5df";
 
     // five day forecast
@@ -59,16 +65,37 @@ export default function Weather (props) {
     setTimeout(() => {  
       axios.get(apiUrl).then(getData); 
     }, 500);
-    
+  }
+  // // // // // //
+
+  // sets state of city variable based on entry in input box
+  function handleEntry(event) {
+    setCity(event.target.value);
   }
 
+  // these three functions get location data and then send current city to search function
+  function updateCurrentLocation(response) {
+    console.log(response.data.address.city);
+    setCity(response.data.address.city);
+    setTimeout(() => {setWeatherData({ready:false})}, 500);
+  }
+  function getPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let locApiKey = "pk.0d4784f4e386eaf7f225cd71379a56ae";
+    let locApiUrl = `https://eu1.locationiq.com/v1/reverse.php?key=${locApiKey}&lat=${latitude}&lon=${longitude}&format=json`
+    axios.get(locApiUrl).then(updateCurrentLocation);
+  }
+  function makeRequest(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(getPosition);
+  }
+  // // // // // //
+
+  // triggers search funtion when form is submitted
   function handleSubmit(event) {
     event.preventDefault();
     search()
-  }
-
-  function handleEntry(event) {
-    setCity(event.target.value)
   }
 
   // runs a search when unit is changed via callback in child component
@@ -76,17 +103,18 @@ export default function Weather (props) {
     search();}, [unit]
   )
 
+  // checks ready status and returns content accordingly
   if (weatherData.ready) {
     return (
       <div className="weatherSearch">
         <form className="form-control pt-3 pb-5" onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-md-6 me-5">
-              <input type="search" placeholder="Enter a city..." onChange={handleEntry}/>
+              <input type="search" placeholder="Enter a city..." value={city} onChange={handleEntry}/>
             </div>
             <div className="col-md-6 ms-5 mb-4">
               <input type="submit" value="Search" className="btn btn-light" />
-              <input type="button" value="Current Location" className="btn btn-dark" />
+              <input type="button" value="Current Location" onClick={makeRequest} rel="noreferrer" className="btn btn-dark" />
             </div>
           </div>
         </form>
